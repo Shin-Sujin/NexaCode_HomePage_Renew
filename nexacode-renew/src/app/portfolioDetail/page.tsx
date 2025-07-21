a8dbec73286fa6249ffe06c803df3afc3566d78a
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2025-07-21 18:27:25 +0900

    design : 포폴 디테일 페이지

--- nexacode-renew/src/app/portfolioDetail/page.tsx
+++ nexacode-renew/src/app/portfolioDetail/page.tsx
@@ -9,6 +9,32 @@ export default function PortfolioDetailPage() {
       className="relative w-screen min-h-screen"
       style={{ width: "100vw", minHeight: "100vh", margin: 0, padding: 0 }}
     >
+      <div
+        className="relative w-full  h-[80vh] min-w-0 min-h-0 flex items-center justify-center xxl:h-[50vh]"
+        style={{ overflowX: "hidden" }}
+      >
+        {/* 배경 이미지 */}
+        <div className="absolute inset-0 w-full h-full z-0">
+          <Image
+            src="/images/nexaBlogImage.jpg"
+            alt="blog_bg"
+            fill
+            style={{ objectFit: "cover" }}
+            priority
+            sizes="100vw"
+          />
+          <div className="absolute inset-0 bg-black/30" />
+        </div>
+        {/* 텍스트 영역 */}
+        <div className="relative z-10 flex flex-col items-center justify-end w-full h-full pb-20 max-pf_md:px-10">
+          <h1 className="text-7xl md:text-5xl font-semibold text-white drop-shadow-lg mb-6 text-center max-pf_md:text-5xl max-pf_xs:text-3xl">
+            Nexa Portfolio
+          </h1>
+          <div className="flex items-center text-white/90 text-lg drop-shadow">
+            <span>넥사코드와 함께한 성공 사례를 공유합니다.</span>
+          </div>
+        </div>
+      </div>
       {/* 실제 콘텐츠 */}
       <div className="relative flex w-screen min-h-screen z-10 max-md:flex-col">
         <div className="w-[55vw] flex items-center justify-end">
