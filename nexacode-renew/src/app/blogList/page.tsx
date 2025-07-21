bd1b47dd897d767fbd634434b86128876d94efa4
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2025-07-21 15:41:36 +0900

    design : 문의하기, 블로그 리스트 반응형

--- nexacode-renew/src/app/blogList/page.tsx
+++ nexacode-renew/src/app/blogList/page.tsx
@@ -9,20 +9,27 @@ import BlogListItem from "@/src/components/blog/BlogListItem";
 export default function BlogListPage() {
   return (
     <div>
-      <main className="max-w-5xl mx-auto px-4 py-48">
-        <h1 className="text-6xl text-gray-800 font-600 mb-6 ">
-          넥사코드 이야기
-        </h1>
-        <h2 className="text-xl text-gray-600 font-400 mb-20 ">
-          넥사코드에서 세상의 변화를 만들어 가고 있는 사람들의 이야기입니다.
-        </h2>
-        <div className="space-y-6">
-          <hr className="flex items-start justify-between border-[0.5px] border-gray-200 y-1" />
+      <main
+        className="w-full max-w-full overflow-x-hidden m-0 p-0"
+        // 또는 style={{ width: "100%", maxWidth: "100vw", overflowX: "hidden", margin: 0, padding: 0 }}
+      >
+        {" "}
+        <main className="max-w-5xl mx-auto px-4 py-48  max-md:w-full max-md:pb-20 ">
+          <h1 className="text-6xl text-gray-800 font-600 mb-6 max-md:text-4xl max-md:px-2 ">
+            넥사코드 이야기
+          </h1>
+          <h2 className="text-xl text-gray-600 font-400 mb-20 max-md:text-lg max-md:px-3 ">
+            넥사코드에서 세상의 변화를 <br className="max-md:block hidden" />
+            만들어 가고 있는 사람들의 이야기입니다.
+          </h2>
+          <div className="space-y-6 max-md:space-y-2">
+            <hr className="flex items-start justify-between border-[0.5px] border-gray-200 y-1 " />
 
-          {blogList.map((item, idx) => (
-            <BlogListItem key={idx} {...item} />
-          ))}
-        </div>
+            {blogList.map((item, idx) => (
+              <BlogListItem key={idx} {...item} />
+            ))}
+          </div>
+        </main>{" "}
       </main>{" "}
       <Footer />
     </div>
--- nexacode-renew/src/components/blog/BlogListItem.tsx
+++ nexacode-renew/src/components/blog/BlogListItem.tsx
@@ -22,24 +22,31 @@ export const BlogListItem = ({
 }: Props) => {
   return (
     <Link href="/blog" className="block">
-      <div className="flex items-start justify-between gap-6 border-b border-gray-200 py-8 cursor-pointer group">
-        <div className="flex-1">
-          <span className="text-sm text-gray-500 font-medium mb-3 block">
-            {category}
-          </span>
-          <h3 className="text-3xl font-500 text-gray-900 mb-2">{title}</h3>
-          <p className="text-gray-500 text-lg">{desc}</p>
-          <div className="text-sm text-gray-400 mt-5">
-            {date} &nbsp;·&nbsp; {author}
-          </div>
-        </div>
-        <div className="w-[300px] h-[187px] flex-shrink-0 relative overflow-hidden">
+      <div className="flex items-start justify-between gap-6 border-b border-gray-200 py-8 cursor-pointer group md:flex-row flex-col max-md:gap-1 max-md:px-4 ">
+        {/* category */}
+        <span className="text-sm text-gray-500 font-medium mb-3 block md:order-1 order-1">
+          {category}
+        </span>
+        {/* Image */}
+        <div className="w-[300px] h-[187px] flex-shrink-0 relative overflow-hidden md:order-3 order-2 max-md:w-full max-md:h-[187px] max-lg:h-[500px] ">
           <Image
             src={thumbnail}
             alt={title}
             fill
             className="object-cover transition-transform duration-300 group-hover:scale-105"
           />
+        </div>
+        {/* Texts */}
+        <div className="flex-1 md:order-2 order-3 flex flex-col">
+          <h3 className="text-3xl font-500 text-gray-900 mb-2 order-1 max-md:text-2xl max-md:mb-1 max-md:mt-2">
+            {title}
+          </h3>
+          <p className="text-gray-500 text-lg order-2 max-md:text-base max-md:hidden">
+            {desc}
+          </p>
+          <div className="text-sm text-gray-400 mt-5 order-3 max-md:text-xs max-md:mt-1">
+            {date} &nbsp;·&nbsp; {author}
+          </div>
         </div>
       </div>
     </Link>
