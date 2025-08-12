aa4eb9c1b259a9801140950567dddcaab02f198d
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2025-08-12 11:40:48 +0900

    light css mobile

--- nexacode-renew/src/components/startPageComponents/ButtonContents02.tsx
+++ nexacode-renew/src/components/startPageComponents/ButtonContents02.tsx
@@ -2,7 +2,7 @@ import "@/src/styles/light.css";
 export default function ButtonContents02() {
   return (
     <div className="flex items-center h-full pb-32">
-      <h2 className="who text-left">
+      <h2 className="who text-left text-white font-black xl:text-8xl text-6xl">
         <span className="who">그렇다면</span>
         <span className="light who">누구에게</span>
         <span className="who"> 맡겨야 할까?</span>
