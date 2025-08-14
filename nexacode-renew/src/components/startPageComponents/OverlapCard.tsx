f02695a51ac95a5e798c137edabd604a4212908c
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2025-08-14 16:59:55 +0900

    overlap 카드 배치 수정

--- nexacode-renew/src/components/startPageComponents/OverlapCard.tsx
+++ nexacode-renew/src/components/startPageComponents/OverlapCard.tsx
@@ -30,34 +30,42 @@ export default function OverlapCard({
         <div
           className={`absolute bottom-0 translate-x-[8rem] translate-y-[1rem] xxl:px-10 xxl:py-12 xxl:w-[26rem] 
         xxl:h-[22rem] xl:px-7 xl:py-10 xl:w-[20rem] xl:h-[16rem] w-[25rem] h-[20rem]
-        ${className}`}
+        ${className} flex flex-col`}
         >
-          <p className="xxl:text-lg xl:text-base xxl:mb-8 xl:mb-5 text-[#555555] ">
-            {category}
-          </p>
-          <h2
-            className="xxl:mb-10 xl:mb-5 xxl:text-2xl xl:text-lg text-gray-800 leading-relaxed break-keep"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-          <hr className="mb-5" />
-          <p className="xxl:text-lg xl:text-sm font-black text-gray-700 mb-2">
-            &quot;{sentence}&quot;
-          </p>
+          <div>
+            <p className="xxl:text-lg xl:text-base xxl:mb-8 xl:mb-5 text-[#555555] ">
+              {category}
+            </p>
+            <h2
+              className="xxl:mb-10 xl:mb-5 xxl:text-2xl xl:text-lg text-gray-800 leading-relaxed break-keep"
+              dangerouslySetInnerHTML={{ __html: title }}
+            />
+          </div>
+          <div className="mt-auto">
+            <hr className="mb-5" />
+            <p className="xxl:text-lg xl:text-sm font-black text-gray-700">
+              &quot;{sentence}&quot;
+            </p>
+          </div>
         </div>
       </div>
       {/* 모바일에서는 따로 컴포넌트 작성 */}
       <div className="xl:hidden flex flex-col w-full max-w-xl justify-center items-center px-auto py-auto">
         {/* 앞에 겹쳐진 흰색 텍스트 박스 */}
-        <div className="bg-white lg:py-20 py-10 px-2 lg:w-[20rem] w-[15rem] lg:h-[23.75rem] h-[20rem] text-2xl  text-center">
-          <p className="text-sm text-[#555555] mb-8">{category}</p>
-          <h2
-            className="lg:mb-16 mb-8 text-lg text-gray-800 leading-relaxed"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-          <hr className="lg:mb-8 mb-5" />
-          <p className="lg:text-lg text-base font-black text-gray-700">
-            {sentence}
-          </p>
+        <div className="bg-white flex flex-col lg:py-20 py-10 px-2 lg:w-[20rem] w-[15rem] lg:h-[23.75rem] h-[20rem] text-2xl  text-center">
+          <div>
+            <p className="text-sm text-[#555555] mb-8">{category}</p>
+            <h2
+              className="text-lg text-gray-800 leading-relaxed break-keep"
+              dangerouslySetInnerHTML={{ __html: title }}
+            />
+          </div>
+          <div className="mt-auto">
+            <hr className="lg:mb-8 mb-5" />
+            <p className="lg:text-lg text-base font-black text-gray-700">
+              {sentence}
+            </p>
+          </div>
         </div>
       </div>
     </>
